2bdd881c8787f175c67a0f02b9d1514c7b4b8eef
Marko Zamurovic <m.zamurovic@icloud.com>
2024-03-27 13:22:43 +0100

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Marko Zamurovic on 3/27/24.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
